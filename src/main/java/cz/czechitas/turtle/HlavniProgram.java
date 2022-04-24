ab5c8ccee5cef7ae32eb8fc88cbd9cae96a8ac83
Michala Lišková <michala.liskova@email.cz>
2022-04-24 21:35:41 +0200

    úkol 02/1

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -8,6 +8,42 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        nakresliObdelnik(zofka, 20, 40);
     }
 
+    private void nakresliRTrojuhlenik (Turtle turtle, double delkaStrany) {
+        for (int i = 0; i < 3; i++) {
+        turtle.move(delkaStrany);
+        turtle.turnRight(120);
+
+        }
+    }
+
+    private void nakresliCtverec(Turtle turtle, double delkaStrany) {
+        for (int i = 0; i < 4; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(90);
+        }
+    }
+
+    private void nakresliMnohohuelnik (Turtle turtle, double delkaStrany, int pocetStran) {
+        double uhel = 360.0 / pocetStran;
+        for (int i = 0; i < pocetStran; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(uhel);
+        }
+    }
+
+    private void nakresliKolecko (Turtle turtle, double delkaStrany)   {
+        nakresliMnohohuelnik(turtle, delkaStrany, 24);
+    }
+
+    private void nakresliObdelnik (Turtle turtle, double delkaStranya, double delkaStranyb) {
+        for (int i = 0; i < 2; i++) {
+        turtle.move(delkaStranya);
+        turtle.turnRight(90);
+        turtle.move(delkaStranyb);
+        turtle.turnRight(90);
+        }
+    }
 }
