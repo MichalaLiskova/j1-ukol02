aa615dd1d86340ae0491e1966a3916b7cbff6e83
Michala Lišková <michala.liskova@email.cz>
2022-04-24 23:54:23 +0200

    úkol 02/2

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,13 +2,75 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
-        nakresliObdelnik(zofka, 20, 40);
+        zofka.setSpeed(100);
+
+        Turtle tomas = new Turtle();
+        tomas.setPenColor(Color.blue);
+
+        Turtle pingu = new Turtle();
+        pingu.setPenColor(Color.darkGray);
+
+
+        //nakreslení sněhuláka - Pingu
+        pingu.setLocation(1100, 200);
+        pingu.turnRight(90);
+        nakresliKolecko(pingu, 8);
+        jdiPulkolecko(pingu, 8);
+        pingu.turnRight(180);
+        nakresliKolecko(pingu, 12);
+        jdiCtvrtkolecko(pingu, 12);
+        pingu.turnLeft(180);
+        nakresliKolecko(pingu, 5);
+        pingu.turnLeft(180);
+        jdiPulkolecko(pingu, 12);
+        pingu.turnLeft(180);
+        nakresliKolecko(pingu, 5);
+        pingu.turnLeft(180);
+        jdiPulkolecko(pingu, 12);
+        jdiCtvrtkolecko(pingu, 12);
+        pingu.turnLeft(180);
+        nakresliKolecko(pingu, 16);
+
+
+
+        //kreslení mašinky - Tomas
+         nakresliObdelnik(tomas, 140, 100);
+         nakresliKolecko(tomas, 13.2);
+         tomas.turnLeft(90);
+         nakresliObdelnik(tomas, 150, 70);
+         tomas.move(110);
+         tomas.turnLeft(180);
+         nakresliKolecko(tomas, 6);
+         tomas.move(70);
+         nakresliKolecko(tomas, 6);
+         tomas.turnLeft(180);
+         tomas.move(110);
+         tomas.penUp();
+         tomas.move(70);
+         tomas.turnLeft(90);
+         tomas.move(30);
+         tomas.turnLeft(45);
+         tomas.penDown();
+         nakresliRamennytrojuhelnik(tomas, 70, 100);
+
+
+        //kreslení zmrzliny - Zofka
+        System.out.println(zofka.getX());
+        System.out.println(zofka.getY());
+        zofka.setLocation(50, 100);
+        zofka.setPenColor(Color.pink);
+        nakresliKolecko(zofka, 20);
+
+        zofka.turnRight(180);
+        nakresliRamennytrojuhelnik(zofka, 200, 150);
     }
 
     private void nakresliRTrojuhlenik (Turtle turtle, double delkaStrany) {
@@ -26,16 +88,27 @@ public class HlavniProgram {
         }
     }
 
-    private void nakresliMnohohuelnik (Turtle turtle, double delkaStrany, int pocetStran) {
+    private void jdiCastMnohohuelniku (Turtle turtle, double delkaStrany, int pocetStran, int cast) {
         double uhel = 360.0 / pocetStran;
-        for (int i = 0; i < pocetStran; i++) {
+
+        turtle.turnRight(uhel/2);
+        for (int i = 0; i < pocetStran/cast; i++) {
             turtle.move(delkaStrany);
             turtle.turnRight(uhel);
         }
+        turtle.turnLeft(uhel/2);
     }
 
     private void nakresliKolecko (Turtle turtle, double delkaStrany)   {
-        nakresliMnohohuelnik(turtle, delkaStrany, 24);
+        jdiCastMnohohuelniku(turtle, delkaStrany, 24, 1);
+    }
+
+    private void jdiPulkolecko (Turtle turtle, double delkaStrany)   {
+        jdiCastMnohohuelniku(turtle, delkaStrany, 24, 2);
+    }
+
+    private void jdiCtvrtkolecko (Turtle turtle, double delkaStrany)   {
+        jdiCastMnohohuelniku(turtle, delkaStrany, 24, 4);
     }
 
     private void nakresliObdelnik (Turtle turtle, double delkaStranya, double delkaStranyb) {
@@ -46,4 +119,21 @@ public class HlavniProgram {
         turtle.turnRight(90);
         }
     }
+
+    private void nakresliRamennytrojuhelnik (Turtle turtle, double delkaRamene, double delkaZakladny) {
+
+            double uhelRamen = 2 * Math.toDegrees(Math.asin((delkaZakladny/2)/delkaRamene));
+            double uhelZakladny = (180 - uhelRamen)/2;
+            turtle.turnLeft(90 - uhelZakladny);
+            turtle.move(delkaRamene);
+            turtle.turnLeft(180 - uhelRamen);
+            turtle.move(delkaRamene);
+            turtle.turnLeft(180 - uhelZakladny);
+            turtle.move(delkaZakladny);
+            turtle.turnLeft(90);
+
+
+    }
+
+
 }
